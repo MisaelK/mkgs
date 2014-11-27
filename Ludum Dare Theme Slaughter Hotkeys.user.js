597517d1afd660e9e5e5b7e4f5ad9ef03906a8c6
MisaelK <misael_k@yahoo.com.ar>
2014-11-27 03:16:31 -0300

    Ludum Dare script now animates on click too.

--- Ludum Dare Theme Slaughter Hotkeys.user.js
+++ Ludum Dare Theme Slaughter Hotkeys.user.js
@@ -1,9 +1,9 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        Ludum Dare Theme Slaughter Hotkeys
 // @namespace   Misael.K
 // @include     http://ludumdare.com/theme/*
 // @require     http://pastebin.com/raw.php?i=UAfz8TrP
-// @version     1
+// @version     1.1
 // ==/UserScript==
 
 // main three links-votes
@@ -27,57 +27,64 @@ $(good).parent().append("<span>press G or +</span>").find("span").css(cssLegend)
 $(bad).parent().append("<span>press B or -</span>").find("span").css(cssLegend);
 $(slaughter).parent().append("<span>press S or 0</span>").find("span").css(cssLegend);
 
+// each vote text-element receives this css
+var cssEnlarge = {
+    "outline": "none",
+    "position": "fixed",
+    "left": "0",
+    "top": "0",
+    "bottom": "0",
+    "right": "0",
+    "margin": "auto -1000%",
+    "height": "0",
+    "line-height": "0",
+    "transform": "rotate(-720deg)",
+    "transition": "1.2s all ease-in 0s",
+    "font-size": $("body").height() + "px"
+};
+
 // vote on keypress
-$(document).on("keypress", function(e) {
-    // each vote text-element receives this css
-    var cssEnlarge = {
-        "position": "fixed",
-        "left": "0",
-        "top": "0",
-        "bottom": "0",
-        "right": "0",
-        "margin": "auto -1000%",
-        "height": "0",
-        "line-height": "0",
-        "transform": "rotate(-720deg)",
-        "transition": "1.2s all ease-in 0s",
-        "font-size": $("body").height() + "px"
-    };
-    
+$(document).on("keypress", function(e) {    
     // after pressing a vote key, shows the animation and changes the 
     // location, effectively voting
     var key = String.fromCharCode(e.charCode);
     // GOOD
     if (key == "g" || key == "+") {
-    $(good).css(cssEnlarge);
-        $(bad).parent().hide();
-        $(slaughter).parent().hide();
-        hideText();
+        animateGood();
         window.location.href = $(good).attr('href');
     }
     // BAD
     if (key == "b" || key == "-") {
-        $(good).parent().hide();
-        $(bad).css(cssEnlarge);
-        $(slaughter).parent().hide();
-        hideText();
+        animateBad();
         window.location.href = $(bad).attr('href');
     }
     // SLAUGHTER
     if (key == "s" || key == "0") {
-        $(good).parent().hide();
-        $(bad).parent().hide();
-        $(slaughter).css(cssEnlarge);
-        hideText();
+        animateSlaughter();
         window.location.href = $(slaughter).attr('href');
-    }
-    
-    // hides everything but the vote
-    function hideText() {
-        var cssHide = {
-            "visibility": "hidden"
-        }
-        $("body").css(cssHide);
     }
 });
 
+// also animate on click
+$(good).on("click", animateGood);
+$(bad).on("click", animateBad);
+$(slaughter).on("click", animateSlaughter);
+
+// animate the selected word and hide the others
+function animateGood() {
+    $(good).css(cssEnlarge);
+    hideText();
+}
+function animateBad() {
+    $(bad).css(cssEnlarge);
+    hideText();
+}
+function animateSlaughter() {
+    $(slaughter).css(cssEnlarge);
+    hideText();
+}
+
+// hides everything but the vote
+function hideText() {
+    $("body").css("visibility", "hidden");
+}
